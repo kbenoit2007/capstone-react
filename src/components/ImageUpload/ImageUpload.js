123569be3e5aac296ae31da733e29f74b0d89d5a
Kevin <kevinb@kevins-mbp.home>
2023-09-27 19:50:14 -0400

    created simple upload for images

---
+++ src/components/ImageUpload/ImageUpload.js
@@ -0,0 +1,63 @@
+import { ChangeEvent, useState } from 'react';
+import axios from "axios"
+
+
+function ImageUpload(){
+    // const [fileList, setFileList] = useState(null)
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileChange = (e) => {
+      setSelectedFile(e.target.files);
+    };
+
+   
+  
+    const handleUpload = () => {
+        if(!selectedFile){
+            return
+        }
+
+
+      // You can implement your image upload logic here, e.g., sending the file to a server.
+      if (selectedFile) {
+        const formData = new FormData();
+        console.log("the selected files "+JSON.stringify(selectedFile))
+        files.forEach((files,i)=>{
+            formData.append(`files`,files, files.name);
+        })
+        // console.log(selectedFile)
+        // console.log(formData)
+       
+  
+        axios.post('http://localhost:3001/upload',formData)
+        //   .then((response) => response.json())
+          .then((data) => {
+            // Handle the response from the server
+            console.log('Server response:', data);
+          })
+          .catch((error) => {
+            console.error('Error uploading image:', error);
+          });
+      }
+    };
+  
+    const files = selectedFile ? [...selectedFile] :[]
+    return (
+        <>
+        
+      <div>
+      <input type="file" onChange={handleFileChange} multiple />
+      <ul>
+        {files.map((file, i) => (
+          <li key={i}>
+            {file.name} - {file.type}
+          </li>
+        ))}
+      </ul>
+        <button onClick={handleUpload}>Upload</button>
+      </div>
+      </>
+    );
+  }
+
+export default ImageUpload;
