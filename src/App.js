b52ef543b4ee55237fc6a05420be4a4e5818ec4e
Kevin <kevinb@kevins-mbp.home>
2023-09-27 23:37:11 -0400

    created routes for React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "./pages/NotFound/NotFound";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import Profile from "./pages/Profile/Profile";
+import Book from "./pages/Book/Book";
+
+//Components
 import ImageUpload from "./components/ImageUpload/ImageUpload";
-import React, { useState } from 'react';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
     <div>Capstone</div>
-<ImageUpload />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="user/:id" element={<Profile />} />
+      <Route path="user/:id/book/:id" element={<Book />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    
+    
+    </BrowserRouter>
+     
+      {/* <ImageUpload /> */}
     </>
-
   );
 }
 
---
+++ src/pages/Book/Book.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Book() {
+  return (
+    <div>
+      <h1>Your Book</h1>
+      <p>Welcome</p>
+    </div>
+  );
+}
+
+export default Book;
